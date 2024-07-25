6f7be667e2658e3413a5c489611d25e7afc64f0d
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-07-25 21:05:54 +0100

    Hello, world!

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables         -*- no-byte-compile: t; -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil      . ((+license/license-choice . "GNU General Public License Version 2")
+              (compile-command         . "make run")))
+ (c++-mode . ((mode                    . clang-format))))
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+/* Copyright (C) 2024 Aryadev Chavali
+
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License Version 2 for
+ * details.
+
+ * You may distribute and modify this code under the terms of the GNU General
+ * Public License Version 2, which you should have received a copy of along with
+ * this program.  If not, please go to <https://www.gnu.org/licenses/>.
+
+ * Created: 2024-07-25
+ * Author: Aryadev Chavali
+ * Description: Entrypoint
+ */
+
+#include <cstdio>
+#include <raylib.h>
+
+int main(void)
+{
+  puts("Hello, world!");
+  return 0;
+}
