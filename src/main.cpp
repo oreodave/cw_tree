0727126061f5ac48984beae72f2f50e92c7e2225
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-07-25 21:32:07 +0100

    Simple fraction structure
    Holds numerator and denominator.  Can self simplify, but not
    automatically.

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,41 @@
  * Description: Entrypoint
  */
 
+#include <cstdint>
 #include <cstdio>
 #include <raylib.h>
+
+typedef uint64_t word_t;
+
+struct Fraction
+{
+  word_t numerator, denominator;
+  bool is_simplified;
+
+  Fraction(word_t numerator = 0, word_t denominator = 1,
+           bool is_simplified = false)
+      : numerator{numerator}, denominator{denominator},
+        is_simplified{is_simplified}
+  {
+  }
+
+  void simplify(void)
+  {
+    // No-Op if already simplified
+    if (is_simplified)
+      return;
+    word_t a = numerator < denominator ? numerator : denominator,
+           b = numerator < denominator ? denominator : numerator;
+    // Euclidean algorithm
+    for (word_t r = b % a; r != 0; b = a, a = r, r = b % a)
+      continue;
+    // a will be the gcd
+    numerator /= a;
+    denominator /= a;
+    // Ensure this is a noop after this
+    is_simplified = true;
+  }
+};
 
 int main(void)
 {
