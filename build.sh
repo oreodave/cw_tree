0008c31f53628d048408efc6a86eee554e3a625d
Aryadev Chavali <aryadev@aryadevchavali.com>
2025-11-27 01:53:10 +0000

    Add src/worker.cpp to build pipeline

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ DFLAGS="-ggdb -fsanitize=address -fsanitize=undefined"
 CFLAGS="$GFLAGS $DFLAGS"
 LIBS="-lraylib -lm"
 
-c++ $CFLAGS -o cw_tree.out src/node.cpp src/state.cpp src/numerics.cpp src/main.cpp $LIBS
+c++ $CFLAGS -o cw_tree.out src/node.cpp src/state.cpp src/worker.cpp src/numerics.cpp src/main.cpp $LIBS
