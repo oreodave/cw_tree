1bd8a9f7b2b258c2bbe65b01cda96855173d50a3
Aryadev Chavali <aryadev@aryadevchavali.com>
2025-11-28 17:21:21 +0000

    Add run component to build script, activated by passing argument `run`

--- build.sh
+++ build.sh
@@ -2,9 +2,15 @@
 
 set -xe
 
+OUT="cw_tree.out"
 GFLAGS="-Wall -Wextra -Wswitch-enum -std=c++17"
 DFLAGS="-ggdb -fsanitize=address -fsanitize=undefined"
 CFLAGS="$GFLAGS $DFLAGS"
 LIBS="-lraylib -lm"
 
-c++ $CFLAGS -o cw_tree.out src/node.cpp src/state.cpp src/worker.cpp src/numerics.cpp src/main.cpp $LIBS
+c++ $CFLAGS -o $OUT src/node.cpp src/state.cpp src/worker.cpp src/numerics.cpp src/main.cpp $LIBS
+
+if [ "$1" = "run" ]
+then
+    ./$OUT
+fi
