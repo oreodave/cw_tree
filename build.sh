82604480c02c0a4ba7d788a8fbe154e20d93dded
Aryadev Chavali <aryadev@aryadevchavali.com>
2025-12-12 05:42:13 +0000

    Setup variable in build script for user to adjust thread timers

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ OUT="cw_tree.out"
 GFLAGS="-Wall -Wextra -Wswitch-enum -std=c++17"
 DFLAGS="-ggdb -fsanitize=address -fsanitize=undefined"
 RFLAGS="-O2"
-CFLAGS="$GFLAGS $DFLAGS"
+VARFLAGS="-DTHREAD_PAUSE_MS=1000 -DTHREAD_GENERAL_MS=1"
+CFLAGS="$GFLAGS $VARFLAGS $DFLAGS"
 LIBS="-lraylib -lm"
 
 build() {
@@ -18,7 +19,7 @@ then
     build && ./$OUT
 elif [ "$1" = "release" ]
 then
-    CFLAGS="$GFLAGS $RFLAGS"
+    CFLAGS="$GFLAGS $VARFLAGS $RFLAGS"
     build;
 else
     build;
